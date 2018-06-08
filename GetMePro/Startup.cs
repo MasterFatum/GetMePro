5dabcbb8406f8057be21438676d06bf671473b9a
Pi Volt <master_Fatum@hotmail.com>
2018-06-08 17:40:23 +0300

    Добавьте файлы проекта.

---
+++ GetMePro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetMePro.Startup))]
+namespace GetMePro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
