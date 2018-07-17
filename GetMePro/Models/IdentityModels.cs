784471a9bc05a3110402d719cc2ceda5319d33e1
Pi Volt <master_Fatum@hotmail.com>
2018-07-17 20:23:31 +0300

    6

--- GetMePro/Models/IdentityModels.cs
+++ GetMePro/Models/IdentityModels.cs
@@ -13,7 +13,7 @@ namespace GetMePro.Models
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-            // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
+            // authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Здесь добавьте утверждения пользователя
             return userIdentity;
